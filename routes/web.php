69da1a1d25ae5f312201488685d7cae4429662bd
kupzed <rizafahdansyahda3@gmail.com>
2025-09-07 20:05:25 +0700

    update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'API OK']);
+    return response()->json([
+        'Message' => 'API OK',
+        'Laravel' => app()->version(),
+    ]);
 });
