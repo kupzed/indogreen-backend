89b2d80d524c0423826a8c8694ae0c8878111fe0
kupzed <rizafahdansyahda3@gmail.com>
2025-11-10 15:21:00 +0700

    add table 'short_desc' in migration activities

--- database/migrations/2025_06_13_075840_create_activities_table.php
+++ database/migrations/2025_06_13_075840_create_activities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('short_desc')->nullable();
             $table->text('description');
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->enum('kategori', [
